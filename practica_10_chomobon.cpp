f44a6a4302987186685f1e3a0aa1fca2a40c3516
Cristina Homobono <c.homobono.2023@alumnos.urjc.es>
2024-12-16 20:03:55 +0100

    Me suma sin querer uno de los numeros que no esta entre a y z

--- practica_10_chomobon.cpp
+++ practica_10_chomobon.cpp
@@ -23,8 +23,11 @@ Fecha: 10/diciembre/2024
 // * main
 int main()
 {
+    bool last_num = 0; //no hay mas numeros
     int sum_num;
+    //int num;
 
+    //num = 0;
     sum_num = 0;
     std::ifstream file("entrada.txt"); //Abre el archivo
     assert(file.is_open()); //Termina el programa en caso de fallo al abrir el archivo
@@ -32,30 +35,37 @@ int main()
 
     while (std::getline(file, line))
     {
-        static const size_t lenerr = -1;
+        static const size_t len_err = -1;
         std::size_t pos1 = line.find('a');
         std::size_t pos2 = line.find('z');
 
-        if(pos1 != lenerr && pos2 != lenerr && pos1 < pos2)
+        if(pos1 != len_err && pos2 != len_err && pos1 < pos2)
         {
-            std::string az_str = line.substr(pos1 + 1, pos2 - pos1 - 1);
-            std::string num;
+            std::string az_str = line.substr(pos1 + 1, pos2 - pos1 - 1); //Donde encuentra la a + 1 = num y pos2 - pos1 -1 me da la pos del num desde la ultima posicion
+            std::string num_as_str;
             for (std::size_t i = 0; i < az_str.length(); i++)
             {
                 char ch_num = az_str[i]; //al ser una string tiene posiciones y por ello las puedo recorrer
                 if (ch_num >= '0' && ch_num <= '9') //Lo uso para ver si es un numero
                 {
-                    num += ch_num;
+                    std::cout << "1nas: " << num_as_str << std::endl;
+                    num_as_str += ch_num;
+                    std::cout << "2nas: " << num_as_str << std::endl;
+                    last_num = 1;
                 }
-                else if(num.length() > 0)
+/*                 else if(last_num)
                 {
-                    sum_num = std::stoi(num);
-                    num = ""; //Lo uso para resetear el numero que esta usando y que no de fallo
+                    sum_num = std::stoi(num_as_str);
+                    std::cout << "tnas: " << num_as_str << std::endl;
+                    num_as_str = ""; //Lo uso para resetear el numero que esta usando y que no de fallo
+                    last_num = 0;
                 }
-            }
-            if (num.length() > 0) //Por si hay un ultimo numero para añadirlo
+            } */
+            if (last_num) //Por si hay un ultimo numero para añadirlo
             {
-                sum_num += std::stoi(num);
+                sum_num += std::stoi(num_as_str);
+                num_as_str = "";
+                last_num = 0;
             }
         }
     }
